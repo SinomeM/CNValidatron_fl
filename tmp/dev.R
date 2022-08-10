f68603bbbc6310d624ca19bfcfad428cf2674af8
Simone Montalbano <simone.montalbano@pm.me>
2022-08-10 11:11:38 +0200

    up

--- tmp/dev.R
+++ tmp/dev.R
@@ -3,7 +3,7 @@ library(ggplot2)
 
 setwd("~/Downloads/CNValidatron_fl")
 cnv <- fread("./data/put_cnv.txt")
-tmpd <- QCtreeCNV:::get_region_tabix(cnv$chr, cnv$start, cnv$end,
+tmpd <- QCtreeCNV:::get_region_tabix(cnv$chr, 0, "",
                                     "./data/toy_snp_array_noCNV.txt.gz")
 tmpc <- fread("./data/put_cnv.txt")
 tmpl <- fread("./data/loci.txt")
@@ -11,4 +11,6 @@ tmpl <- fread("./data/loci.txt")
 source("./R/create_image.R")
 tmp <- cnv_image_matrix(dt = tmpd, region = tmpl, n = 32, eps = 2,
                         sides = 4, adj = T, path_img = "./tmp/prova.png")
+tmp <- cnv_image_matrix(dt = tmpd, region = tmpl, n = 51, eps = 1,
+                        sides = 20, adj = T, path_img = "./tmp/prova2.png")
 tmp
