a5caa7dafaa434b20cbf4ec28e87ad464821fe68
Simone Montalbano <48947872+SinomeM@users.noreply.github.com>
2022-08-10 20:06:08 +0200

    fixed the 0:n vs 1:n issue

--- R/create_image.R
+++ R/create_image.R
@@ -16,6 +16,8 @@ cnv_image_matrix <- function(dt, region, n, eps = 4, sides = 0.5, adj = T, path_
   # check if eps is even, n must be even as well
   if ((n %% 2) == 0) & (eps %% 2) != 0) stop("eps must be even if n is even")
   if ((n %% 2) != 0) & (eps %% 2) == 0) stop("eps must be odd if n is odd")
+  
+  if (eps > 0.1*n) stop("eps value should not exeed 10% of n")
 
   # extract the relevant markers
   ch <- region$chr; st <- region$start; en <- region$end; len <- en-st+1
@@ -31,29 +33,29 @@ cnv_image_matrix <- function(dt, region, n, eps = 4, sides = 0.5, adj = T, path_
   lrr <- dt[ , .(position, LRR)]
   baf <- dt[ , .(position, BAF)]
 
-  # two n*np matrix
+  # two n*np tables descirbing the actual matrix, x is [0,n-1], y [0,np-1]
   np <- round((n-eps) / 2)
+  lrr[, x := ((position-a) / (b-a)) * (n-1) ][, y := ((LRR+2) / 4) * (np-1)]
+  baf[, x := ((position-a) / (b-a)) * (n-1) ][, y := BAF * (np-1)]
 
-  lrr[, x := ((position-a) / (b-a)) * n ][, y := ((LRR+2) / 4) * np]
-  baf[, x := ((position-a) / (b-a)) * n ][, y := BAF * np]
-
-  # compose the final matrix
   npp <- n/2 + eps
   lrr[, y := y + npp]
-
   dtm <- round(rbind(lrr[, .(x, y)], baf[, .(x, y)]))
+  
+  # in the n*n table x and y are [1,n]
+  dtm[, x := x+1][, y := y+1]
 
+  # count the value of each "pixel" and scale it to [0,1]
   dtm <- dtm[, .N, by= list(x,y)]
-
-  # scale it to [0,1]
   dtm[, N := ((N-min(dtm$N, na.rm = T)) / max(dtm$N, na.rm = T))]
 
-  # create all possible x y positions
+  # create all possible x y positions, [1,n]
   tmp <- as.data.table(gtools::permutations(n, 2))
   colnames(tmp) <- c("x", "y")
   # the diagonal will be missing
   tmp <- rbind(tmp, data.table(x = 1:n, y = 1:n))
 
+  # create the final table of pixel values and coordinates
   dtm <- merge(tmp, dtm, all.x=T)
   # change NAs to 0s
   dtm[is.na(N), N := 0]
