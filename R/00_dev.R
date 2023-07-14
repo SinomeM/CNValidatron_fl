6cbbf277a16ffa87f8659af4bd522c8605656f21
Simone Montalbano <simone.montalbano@pm.me>
2023-07-14 09:59:10 +0200

    up

--- R/00_dev.R
+++ R/00_dev.R
@@ -1,12 +1,17 @@
 # install latest QCtreeCNV
 if (F) devtools::install_github("sinomem/QCtreeCNV")
 
+setwd('~/Documents/CNValidatron_fl')
+
 # libs
 library(data.table)
 
 # Load CNV and samples table from UKB export
-cnvs <- fread('~/Documents/UKB_data/export/cnvs.txt')
-samples <- fread('~/Documents/UKB_data/export/samples.txt')
+cnvs <- fread('~/Documents/UKB_data/edited/cvns.txt')
+samples <- fread('~/Documents/UKB_data/edited/samples.txt')
+loci <- fread('~/Documents/UKB_data/edited/loci.txt')
+
+cnvs <- cnvs[numsnp >= 20,]
 
 # check everything is fine
 all(cnvs$sample_ID %in% samples$sample_ID)
@@ -16,11 +21,11 @@ library(torch)
 cuda_is_available()
 
 # test the function to create the PNG plots of the CNV calls
-cnvs[, local_fp := paste0('~/Documents/UKB_data/export/tabix/', sample_ID, '.tabix.gz')]
-cnvs[, chr := gsub('chr', '', chr)]
-a <- cnvs[sample(1:nrow(cnvs), 1)]
-int_data <- QCtreeCNV:::get_region_tabix(a$chr, a$start, a$stop, a$local_fp)
-# get_region_tabix seems to work
-
 source('./R/create_image.R')
-cnv_image_matrix()
+a <- cnvs[sample(1:nrow(cnvs), 1)]
+a
+int_data <- QCtreeCNV:::get_region_tabix(a$chr, 0, 1000000000,
+                                         samples[sample_ID == a$sample_ID, file_path_tabix] )
+
+cnv_image_matrix(dt = int_data, region = a, n = 50, eps = 4, sides = 0.5,
+                 adj = T, path_img = 'tmp/prova3.png')
--- R/create_image.R
+++ R/create_image.R
@@ -1,11 +1,13 @@
 #' Create a n*n machine-freindly image of the classic LRR BAF plots
 #'
+#' @param dt the output of `QCtreeCNV:::get_region_tabix()`
+#' @param region one line data.table with CNV position information
 #' @param n image dimension in pixels
-#' @dt the output of `QCtreeCNV:::get_region_tabix()`
-#' @region one line data.table with CNV position information
-#' @n number of pixel per side of the final matrix
-#' @eps epsilon, number of empty pixels "between" the two plots, must be even
-#' @sides proportion of the CNV length in each side region (simmetric)
+#' @param n number of pixel per side of the final matrix
+#' @param eps epsilon, number of empty pixels "between" the two plots, must be even
+#' @param sides proportion of the CNV length in each side region (simmetric)
+#' @param adj use `LRRadj` or not
+#' @param path_img where to save the PNG
 #'
 #' @import data.table
 #'
@@ -16,7 +18,7 @@ cnv_image_matrix <- function(dt, region, n, eps = 4, sides = 0.5, adj = T, path_
   # check if eps is even, n must be even as well
   if ((n %% 2) == 0 & (eps %% 2) != 0) stop("eps must be even if n is even")
   if ((n %% 2) != 0 & (eps %% 2) == 0) stop("eps must be odd if n is odd")
-  
+
   if (eps > 0.1*n) stop("eps value should not exeed 10% of n")
 
   # extract the relevant markers
@@ -24,7 +26,7 @@ cnv_image_matrix <- function(dt, region, n, eps = 4, sides = 0.5, adj = T, path_
   a <- st - (len*sides); b <- en + (len*sides)
 
   dt <- dt[chr == ch & between(position, a, b), ]
-  
+
   if (adj) dt[, LRR := LRRadj]
 
   # LRR values higher than 2 are set to 2, lower than -2 to -2
@@ -41,7 +43,7 @@ cnv_image_matrix <- function(dt, region, n, eps = 4, sides = 0.5, adj = T, path_
   npp <- n/2 + eps
   lrr[, y := y + npp]
   dtm <- round(rbind(lrr[, .(x, y)], baf[, .(x, y)]))
-  
+
   # in the n*n table x and y are [1,n]
   dtm[, x := x+1][, y := y+1]
 
