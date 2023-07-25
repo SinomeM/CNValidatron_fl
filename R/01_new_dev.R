0ac39c063b282833ee7a3ba282e150d8454853d8
Simone Montalbano <simone.montalbano@pm.me>
2023-07-25 14:24:44 +0200

    early testing complete

--- R/01_new_dev.R
+++ R/01_new_dev.R
@@ -256,7 +256,7 @@ convnet_dropout <- nn_module(
       nn_relu(),
       nn_max_pool2d(kernel_size = 2),
       nn_dropout2d(p = 0.05),
-      nn_conv2d(npx*8, npx*16, kernel_size = 3, padding = 1), 
+      nn_conv2d(npx*8, npx*16, kernel_size = 3, padding = 1),
       nn_relu(),
       nn_adaptive_avg_pool2d(c(1, 1)),
       nn_dropout2d(p = 0.05),
@@ -298,7 +298,7 @@ convnet_batchnorm <- nn_module(
       nn_batch_norm2d(npx*8),
       nn_relu(),
       nn_max_pool2d(kernel_size = 2),
-      nn_conv2d(npx*8, npx*16, kernel_size = 3, padding = 1), 
+      nn_conv2d(npx*8, npx*16, kernel_size = 3, padding = 1),
       nn_batch_norm2d(npx*16),
       nn_relu(),
       nn_adaptive_avg_pool2d(c(1, 1)),
@@ -329,8 +329,8 @@ model <- convnet_dropout %>%
       luz_metric_accuracy()
     )
   )
-rates_and_losses <- model %>% lr_finder(train_dl, end_lr = 0.3)
-rates_and_losses %>% plot() # REMEMBER TO CHECK AND UPDATE max_lr !!!!!
+rates_and_losses_do <- model %>% lr_finder(train_dl, end_lr = 0.3)
+rates_and_losses_do %>% plot() # REMEMBER TO CHECK AND UPDATE max_lr !!!!!
 # fit
 fitted_dropout <- model %>%
   fit(train_dl, epochs = 50, valid_data = valid_dl,
@@ -356,8 +356,8 @@ model <- convnet_batchnorm %>%
       luz_metric_accuracy()
     )
   )
-rates_and_losses <- model %>% lr_finder(train_dl, end_lr = 0.3)
-rates_and_losses %>% plot() # REMEMBER TO CHECK AND UPDATE max_lr !!!!!
+rates_and_losses_bn <- model %>% lr_finder(train_dl, end_lr = 0.3)
+rates_and_losses_bn %>% plot() # REMEMBER TO CHECK AND UPDATE max_lr !!!!!
 
 fitted_batchnorm <- model %>%
   fit(train_dl, epochs = 50, valid_data = valid_dl,
@@ -365,7 +365,7 @@ fitted_batchnorm <- model %>%
         luz_callback_early_stopping(patience = 5),
         luz_callback_lr_scheduler(
           lr_one_cycle,
-          max_lr = 0.001,
+          max_lr = 0.00001,
           epochs = 50,
           steps_per_epoch = length(train_dl),
           call_on = "on_batch_end"),
@@ -373,6 +373,130 @@ fitted_batchnorm <- model %>%
         luz_callback_csv_logger("logs_batchnorm.csv")
         ),
       verbose = TRUE)
+# even at low max_lr it seems this model tends to overfit at the moment,
+# maybe it will be more helpful when I have a larger dataset
 
 
 # --------------------------------------------------------------------------- #
+
+
+
+
+## Model training start to end ##
+
+# All lines to test the model(s) training should be here
+setwd('~/Documents/CNValidatron_fl')
+library(data.table)
+library(ggplot2)
+library(torch)
+library(torchvision)
+library(torchdatasets)
+library(luz)
+source('./R/02_new_dev_functions.R')
+
+# Load CNV and samples table from UKB export
+cnvs <- fread('~/Documents/UKB_data/edited/cvns.txt')
+samples <- fread('~/Documents/UKB_data/edited/samples.txt')
+loci <- fread('~/Documents/UKB_data/edited/loci.txt')
+snps <- fread('~/Documents/UKB_data/snppos_filtered.txt')
+cnvs <- cnvs[numsnp >= 20,]
+# load visual validations
+tmp <- list.files('~/Documents/UKB_data/visual_inspection2/')
+tmp <- tmp[grep('visual', tmp)]
+vi_cnv <- data.table()
+for (i in tmp)
+  vi_cnv <- rbind(vi_cnv,
+                  fread(paste0('~/Documents/UKB_data/visual_inspection2/', i)))
+
+# only larger CNVs for now
+tmp <- vi_cnv[numsnp > 40 & Visual_Output %in% 1:3, ]
+train_test <- tmp[sample(1:nrow(tmp), round(nrow(tmp)*0.7) )]
+valid_test <- fsetdiff(tmp, train_test)
+
+# save plots
+npx = 64
+train_pt <- '/home/simone/Documents/CNValidatron_fl/tmp/train'
+valid_pt <- '/home/simone/Documents/CNValidatron_fl/tmp/valid'
+save_pngs_dataset(train_pt, train_test, samples, snps, w = npx, flip_chance = 0.7)
+save_pngs_dataset(valid_pt, valid_test, samples, snps, w = npx, flip_chance = 0.7)
+
+# training and validation dataloaders
+train_dl <- dataloader(torchvision::image_folder_dataset(
+                         train_pt, transform = . %>% transform_to_tensor()),
+                       batch_size = 128, shuffle = TRUE)
+valid_dl <- dataloader(torchvision::image_folder_dataset(
+                         valid_pt, transform = . %>% transform_to_tensor()),
+                       batch_size = 128, shuffle = TRUE)
+
+npx
+classes <- 5 # T del/dup, U del/dup, F
+
+# at the moment this seems the best easy model
+convnet_dropout <- nn_module(
+  "convnet",
+  initialize = function() {
+    self$features <- nn_sequential(
+      nn_conv2d(3, npx, kernel_size = 3, padding = 1),
+      nn_relu(),
+      nn_max_pool2d(kernel_size = 2),
+      nn_dropout2d(p = 0.05),
+      nn_conv2d(npx, npx*2, kernel_size = 3, padding = 1),
+      nn_relu(),
+      nn_max_pool2d(kernel_size = 2),
+      nn_dropout2d(p = 0.05),
+      nn_conv2d(npx*2, npx*4, kernel_size = 3, padding = 1),
+      nn_relu(),
+      nn_max_pool2d(kernel_size = 2),
+      nn_dropout2d(p = 0.05),
+      nn_conv2d(npx*4, npx*8, kernel_size = 3, padding = 1),
+      nn_relu(),
+      nn_max_pool2d(kernel_size = 2),
+      nn_dropout2d(p = 0.05),
+      nn_conv2d(npx*8, npx*16, kernel_size = 3, padding = 1),
+      nn_relu(),
+      nn_adaptive_avg_pool2d(c(1, 1)),
+      nn_dropout2d(p = 0.05),
+    )
+    self$classifier <- nn_sequential(
+      nn_linear(npx*16, npx*16),
+      nn_relu(),
+      nn_dropout(p = 0.05),
+      nn_linear(npx*16, npx*16),
+      nn_relu(),
+      nn_dropout(p = 0.05),
+      nn_linear(npx*16, classes)
+    )
+  },
+  forward = function(x) {
+    x <- self$features(x)$squeeze()
+    x <- self$classifier(x)
+    x
+  }
+)
+
+# learning rate finder
+model <- convnet_dropout %>%
+  setup(
+    loss = nn_cross_entropy_loss(),
+    optimizer = optim_adam,
+    metrics = list(
+      luz_metric_accuracy()
+    )
+  )
+rates_and_losses_do <- model %>% lr_finder(train_dl, end_lr = 0.3)
+rates_and_losses_do %>% plot() # REMEMBER TO CHECK AND UPDATE max_lr !!!!!
+# fit
+fitted_dropout <- model %>%
+  fit(train_dl, epochs = 50, valid_data = valid_dl,
+      callbacks = list(
+        luz_callback_early_stopping(patience = 5),
+        luz_callback_lr_scheduler(
+          lr_one_cycle,
+          max_lr = 0.01,
+          epochs = 50,
+          steps_per_epoch = length(train_dl),
+          call_on = "on_batch_end"),
+        luz_callback_model_checkpoint(path = "cpt_dropout/"),
+        luz_callback_csv_logger("logs_dropout.csv")
+        ),
+      verbose = TRUE)
