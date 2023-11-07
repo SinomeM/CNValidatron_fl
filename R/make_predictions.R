94d578645970a3bcba03e5a204799067da21e5a2
Simone Montalbano <simone.montalbano@pm.me>
2023-11-07 13:58:03 +0100

    nice update, predictions are now given as proper probabilities and I fixed a typo

--- R/make_predictions.R
+++ R/make_predictions.R
@@ -7,6 +7,8 @@
 #' @param model pre trained model loaded using `luz::luz_load()`
 #' @param root root folder for the dataset, created using `save_pngs_prediction()`
 #' @param cnvs cnv data.table in the usual format
+#' @param return_pred_dt returt the prediction table before merging w/ the cnv table, set
+#'          it to T if there is a problem with the `sample_ID`
 #'
 #' @export
 #'
@@ -15,13 +17,13 @@
 #' @import luz
 #' @import torch
 
-make_predicitons <- function(model, root, cnvs) {
+make_predictions <- function(model, root, cnvs, return_pred_dt = F) {
 
   pred_dt <- image_folder_dataset(root, transform = . %>% transform_to_tensor())
   # the output is raw logits
   pred_tens <- predict(model, pred_dt)
   # convert to probabilities
-  pred_tens <- 1 / (1 + exp(-pred_tens))
+  pred_tens <- nnf_softmax(pred_tens, dim = 2)
 
   # the classes are
   # false:   1
@@ -30,16 +32,19 @@ make_predicitons <- function(model, root, cnvs) {
   # unk_del: 4
   # unk_dup: 5
 
+  pred_ix <- as.matrix(torch_argmax(pred_tens, dim = 2))[, 1]
+  pred_probs <- round(as.matrix(pred_tens), 3)
 
-  pred_dt <- data.table(ix = pred_dt$samples[[1]],
-                        class1 = round(as.numeric(pred_tens[,1]), 5),
-                        class2 = round(as.numeric(pred_tens[,2]), 5),
-                        class3 = round(as.numeric(pred_tens[,3]), 5),
-                        class4 = round(as.numeric(pred_tens[,4]), 5),
-                        class5 = round(as.numeric(pred_tens[,5]), 5))
+  pred_dt <- data.table(ix = pred_dt$samples[[1]], pred = pred_ix)
 
   for (i in 1:nrow(pred_dt))
-    pred_dt[i, pred := which.max(c(class1, class2, class3, class4, class5))]
+    pred_dt[i, pred_prob := pred_probs[i, pred_ix[i]]]
+
+  pred_probs <- as.data.table(pred_probs)
+  colnames(pred_probs) <- paste0('prob', 1:5)
+  pred_dt <- cbind(pred_dt, pred_probs)
+
+  return(pred_dt)
 
   pred_dt[, sample_ID := gsub('.+new/', '', ix)][,
             start := gsub('\\d+_', '', sample_ID)][,
--- tmp/dev.R
+++ tmp/dev.R
@@ -13,4 +13,18 @@ devtools::build()
 
 devtools::install()
 
+library(data.table)
+pt <- '~/Documents/CNValidatron_fl/tmp/prova'
+samps <- fread('~/Documents/UKB_data/edited/samples.txt')[sample(200),]
+cnvs <- fread('~/Documents/CNValidatron_trained_models/all_visual_inspection_min40snps.txt')[
+          sample_ID %in% samps$sample_ID,]
+snps <- fread('~/Documents/UKB_data/snppos_filtered.txt')
+
+save_pngs_prediction(pt, cnvs, samps, snps,  w = 64)
+
+
+devtools::load_all()
+pred <- make_predictions(luz::luz_load('~/Documents/CNValidatron_trained_models/fitted_dropout_all.rds'),
+                         pt, cnvs)
+pred
 
