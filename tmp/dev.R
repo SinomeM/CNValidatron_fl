b127ee0fa85fa5abbdd3c4f3796355a04fa02d0e
Simone Montalbano <simone.montalbano@pm.me>
2024-03-06 15:26:22 +0100

    up

--- tmp/dev.R
+++ tmp/dev.R
@@ -39,14 +39,11 @@ plot_cnv(a, b)
 
 
 library(data.table)
-cnvs <- fread('../../UKB_GW_CNVs/cnvs_with_cnvrs.txt')
-
 devtools::load_all()
-# debugonce(cnvrs_iou)
-dt <- cnvrs_iou(cnvs, QCtreeCNV::hg19_chr_arms)
-dt[[2]][, summary(n)]
-a <- dt[[2]]
-a[n>500, ]
-b <- dt[[1]]
-b[CNVR == '1p_27_6', ][, summary(center)]
-b[CNVR == '1p_27_6', ][, summary(length)]
+
+cnvs <- fread('../../UKB_GW_CNVs/cnvs_pred.txt')[pred %in% 2:3 & pred_prob >= 0.9, ]
+# mild filters on outliers
+cnvs <- cnvs[length <= 10000000, ]
+cnvs <- cnvs[!sample_ID %in% cnvs[ , .N, by = sample_ID][N >10, sample_ID], ]
+fwrite(cnvs[, .(chr, start, end, GT)], '../../UKB_GW_CNVs/igv/cnvs.bed', col.names = F, sep = '\t')
+
