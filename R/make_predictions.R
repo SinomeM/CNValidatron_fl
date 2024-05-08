d3fbd76ae06d6b72cb5ccc48ba4c0500ab45b303
Simone Montalbano <simone.montalbano@pm.me>
2024-05-08 14:05:47 +0200

    png in batches works now

--- R/make_predictions.R
+++ R/make_predictions.R
@@ -19,55 +19,47 @@
 
 make_predictions <- function(model, root, cnvs, return_pred_dt = F, batches = 1000) {
 
-  dt_rbind <- data.table()
 
-  for (bb in 1:batches) {
-    message(paste0('Batch ', bb, ' of ', batches))
-    pt <- paste0(root, '/batch', bb, '/')
+  pred_dt <- image_folder_dataset(root, transform = . %>% transform_to_tensor())
+  # the output is raw logits
+  pred_tens <- predict(model, pred_dt)
+  # convert to probabilities
+  pred_tens <- nnf_softmax(pred_tens, dim = 2)
 
-    pred_dt <- image_folder_dataset(pt, transform = . %>% transform_to_tensor())
-    # the output is raw logits
-    pred_tens <- predict(model, pred_dt)
-    # convert to probabilities
-    pred_tens <- nnf_softmax(pred_tens, dim = 2)
+  # the classes are
+  # false:   1
+  # tru_del: 3
+  # tru_dup: 3
 
-    # the classes are
-    # false:   1
-    # tru_del: 3
-    # tru_dup: 3
+  pred_ix <- as.matrix(torch_argmax(pred_tens, dim = 2))[, 1]
+  pred_probs <- round(as.matrix(pred_tens), 3)
 
-    pred_ix <- as.matrix(torch_argmax(pred_tens, dim = 2))[, 1]
-    pred_probs <- round(as.matrix(pred_tens), 3)
+  pred_dt <- data.table(ix = pred_dt$samples[[1]], pred = pred_ix)
 
-    pred_dt <- data.table(ix = pred_dt$samples[[1]], pred = pred_ix)
+  for (i in 1:nrow(pred_dt))
+    pred_dt[i, pred_prob := pred_probs[i, pred_ix[i]]]
 
-    for (i in 1:nrow(pred_dt))
-      pred_dt[i, pred_prob := pred_probs[i, pred_ix[i]]]
-
-    pred_probs <- as.data.table(pred_probs)
-    colnames(pred_probs) <- c('p_false', 'p_true_del', 'p_true_dup', 'p_unk_del', 'p_unk_dup')
-    pred_dt <- cbind(pred_dt, pred_probs)
-
-    dt_rbind <- rbind(dt_rbind, pred_dt)
-  }
+  pred_probs <- as.data.table(pred_probs)
+  colnames(pred_probs) <- c('p_false', 'p_true_del', 'p_true_dup')
+  pred_dt <- cbind(pred_dt, pred_probs)
 
 
-  dt_rbind[, sample_ID := gsub('samp\\w+', '', ix)][,
+  pred_dt[, sample_ID := gsub('.+samp', '', ix)][,
             sample_ID := as.character(gsub('_.+', '', sample_ID))][,
-            start := gsub('_st\\d+', '', ix)][,
-            start := as.integer(gsub('_st', '', start))][,
-            real_numsnp := gsub('nsnp\\w+', '', ix)][,
-            real_numsnp := as.integer(gsub('nsnp', '', real_numsnp))]
+            start := gsub('.+st', '', ix)][,
+            start := as.integer(gsub('_.+', '', start))][,
+            real_numsnp := gsub('.+nsnp', '', ix)][,
+            real_numsnp := as.integer(gsub('\\.png', '', real_numsnp))]
 
   if (return_pred_dt)
-    returt(dt_rbind)
+    returt(pred_dt)
 
-  dt_rbind[, ix := NULL]
+  pred_dt[, ix := NULL]
   cnvs[, ':=' (sample_ID = as.character(sample_ID), start = as.integer(start))]
 
-  dt_rbind <- merge(dt_rbind, cnvs[, .(sample_ID, chr, start, end, numsnp,
+  pred_dt <- merge(pred_dt, cnvs[, .(sample_ID, chr, start, end, numsnp,
                                 length, conf, GT, CN)],
                    by = c('sample_ID', 'start'))
 
-  returt(dt_rbind)
+  returt(pred_dt)
 }
--- R/save_png_for_prediction.R
+++ R/save_png_for_prediction.R
@@ -18,7 +18,6 @@ save_pngs_prediction <- function(root, cnvs, samps, snps, shrink_lrr = 0.2,
   if (dir.exists(root)) warning('Root folder already exists!')
 
   dir.create(root)
-  #dir.create(paste0(root, '/new'))
 
   if (!'batch' %in% colnames(cnvs)) cnvs[, batch := sample(1:batches, .N, replace = T)]
   for (i in 1:batches) dir.create(paste0(root, '/batch', i), showWarnings = F)
@@ -31,23 +30,14 @@ save_pngs_prediction <- function(root, cnvs, samps, snps, shrink_lrr = 0.2,
     n_real_snps <- dt[[2]]
     dt <- dt[[1]]
 
-    pt <- paste0(root, '/new/batch', a$batch, '/samp', a$sample_ID, '_st', a$start,
+    pt <- paste0(root, '/batch', a$batch, '/samp', a$sample_ID, '_st', a$start,
                  '_nsnp', n_real_snps, '.png')
 
-    #if (!file.exists(pt)) {
-    #  if (nrow(dt) == 0) {
-    #    warning('no image saved for cnv: ', a)
-    #    return(data.table())
-    #}
-
     imager::save.image(imager::as.cimg(dt), pt)
-    #}
 
     if (x %% 100 == 0) gc()
   }
 
   if (no_parall)  null <- lapply(1:nrow(cnvs), FUN)
   else null <- BiocParallel::bplapply(1:nrow(cnvs), FUN)
-  # save images using BiocParallel
-
 }
