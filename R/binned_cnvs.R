645fb5f4ff696cdf75375845c7e48e269e96d90d
Simone Montalbano <simone.montalbano@pm.me>
2024-06-18 12:11:43 +0200

    updated binning function,  removed NA fomr count table

--- R/binned_cnvs.R
+++ R/binned_cnvs.R
@@ -3,31 +3,27 @@
 #' This can be regular windows throughout the genome or exons / genes for example.
 #'
 #' @param cnvs usual CNV `data.table`
-#' @param format format for the output table, "wide" and "biot" return a list
+#' @param format format for the output table, "count" and "both" return a list
 #' @param bins for computing the overlap, by default are regular bins created by the helper function `binned_genome`
 #'
 #' @import data.table
 #' @export
 
 binned_cnvs <- function(cnvs, format = c('long', 'count', 'wide', 'both'),
-                        bins = binned_genome(chrs_sten = QCtreeCNV::hg19_start_end_centromeres, bin_size = 100000, chrs = 1:22)) {
-
-  dt <- bins
+                        bins = binned_genome(chrs_sten = QCtreeCNV::hg19_start_end_centromeres,
+                                             bin_size = 100000, chrs = 1:22)) {
 
   # compute the table in "long format", one line per CNV-bin match
-  dto <- data.table()
-  for (i in dt[, unique(chr)]) {
-    dtx <- cnvs[chr == i, .(chr, start, end, sample_ID, GT)]
-    dty <- dt[chr == i, ]
-    setkey(dtx, start, end)
-    setkey(dty, start, end)
-
-    dto <- rbind(dto, unique(foverlaps(dtx, dty)))
-  }
+  dtx <- cnvs[, .(chr, start, end, sample_ID, GT)]
+  dty <- bins
+  setkey(dtx, chr, start, end)
+  setkey(dty, chr, start, end)
+  dto <-  unique(foverlaps(dtx, dty))
 
   if (format == 'long') return(dto)
   # add the count table, one line per "variant"
-  if (format == 'count') return(list(dto, dto[, .N, by = c('chr', 'start', 'end', 'GT')]))
+  if (format == 'count')
+    return(list(dto, dto[!is.na(chr), ][, .N, by = c('chr', 'start', 'end', 'GT')]))
 
   # create the table in the wide format, one column per sample
   dtow <- dcast(dto, ix + chr + start + end ~ sample_ID, value.var = 'GT')
