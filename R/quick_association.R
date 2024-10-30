03701e748c2f5269089c627ced94fc77db131705
Simone Montalbano <simone.montalbano@pm.me>
2024-10-30 17:16:19 +0000

    up

--- R/quick_association.R
+++ R/quick_association.R
@@ -7,6 +7,8 @@
 #'
 #'
 #' @import data.table
+#'
+#' @export
 
 
 quick_assoc <- function(markers, pheno, test = 'fisher') {
@@ -67,4 +69,105 @@ exclude_fixed_loci <- function(assoc_res, loci, boundary = 200000) {
   }
   dt_no_f <- fsetdiff(assoc_res, dt_f)
   return(list(in_loci = dt_f, not_in_loci = dt_no_f))
+}
+
+
+
+#' Process results from quick assoc
+#'
+#' Process `quick_assoc()` outputs for easier visualization.
+#' Perform p value adjustment, filter based on max p value
+#' and exclude fixed loci.
+#'
+#'
+#' @import data.table
+#'
+#' @export
+
+process_quick_res <- function(quick_res, markers, padj = F, max_pval = 0.1,
+                              loci, loci_boundaries = 250000) {
+  if (padj) {
+    quick_res[, adj_pval := signif(p.adjust(pval, method = 'fdr'), 3)]
+    quick_res <- quick_res[adj_pval <= max_pval,]
+  }
+  else quick_res <- quick_res[pval <= max_pval,]
+
+  dt <- merge(quick_res, unique(markers[, .(marker_ID, chr, start, end)]),
+              by = 'marker_ID')
+  res <- CNValidatron:::exclude_fixed_loci(dt, loci, 250000)
+}
+
+
+
+
+#' Run logisti regression on a set of CNV markers
+#'
+#'
+#'
+#'
+#' @import data.table
+#'
+#' @export
+
+run_assoc_logistic <- function(markers, scan_res, pheno, padj = F,
+                               max_pval = 0.1, min_carriers = 5) {
+
+  # re unique to be always sure
+  markers <- unique(markers)
+  scan_res <- unique(scan_res)
+  pheno <- unique(pheno)
+
+  # select markers
+  if (padj)
+    dt <- markers[marker_ID %in% scan_res[adj_pval <= max_pval, marker_ID], ]
+  else
+    dt <- markers[marker_ID %in% scan_res[pval <= max_pval, marker_ID], ]
+
+  all_markers <- dt[, unique(marker_ID)]
+  l_m <- length(all_markers)
+
+  # run logistic regression for each selected marker
+  res <- data.table()
+  for (i in 1:l_m) {
+    message(i, ' of ', l_m, ' markers')
+    mm <- all_markers[i]
+    dtm <- dt[marker_ID == mm, ]
+
+    # number of carriers
+    n_del <- dtm[GT == 1, .N]
+    n_dup <- dtm[GT == 2, .N]
+    # if none above 5 skip marker
+    if (n_del < min_carriers & n_dup < min_carriers) next
+
+    # pheno file for the marker
+    tmp <- pheno[, .(sample_ID, pheno, age, gender)]
+    if (n_del < min_carriers)
+      tmp[sample_ID %in% dtm[GT == 1, sample_ID], var := 1]
+    if (n_dup < min_carriers)
+      tmp[sample_ID %in% dtm[GT == 2, sample_ID], var := 2]
+    tmp[is.na(var), var := 0]
+
+    # HERE DUP carriers will be treated as controls if the DEL has
+    # more than 5 but the DUP not. In GDK is done differently
+
+    # run association
+    mod <- gam(pheno ~ var + s(age) + gender, data = tmp, family = binomial)
+
+    # format results
+    pt <- summary(mod)$p.table
+    tmp <- cbind(rownames(pt), as.data.table(pt))
+    colnames(tmp) <- c('var', 'log_OR', 'SE', 'Z', 'pval')
+    if (n_del >= 5)
+      res <- rbind(res, data.table(marker_ID = mm, GT = 1, n<-car = n_del,
+                                   log_OR = signif(tmp[var == 'var1', log_OR], 3),
+                                   SE = signif(tmp[var == 'var1', SE], 3),
+                                   pval = signif(tmp[var == 'var1', pval], 3)))
+    if (n_dup >= 5)
+      res <- rbind(res, data.table(marker_ID = mm, GT = 2, n<-car = n_del,
+                                   log_OR = signif(tmp[var == 'var2', log_OR], 3),
+                                   SE = signif(tmp[var == 'var2', SE], 3),
+                                   pval = signif(tmp[var == 'var2', pval], 3)))
+
+  }
+  return(list(res, markers, pheno))
 }
